52e8c478ddb4df2b736a17c5fe6e90350e772c39
Areon Lundkvist <areon.lundkvist@gmail.com>
2021-05-17 20:43:29 +0100

    ci

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class BookController extends Controller
 {
--- app/Http/Controllers/HighscoreController.php
+++ app/Http/Controllers/HighscoreController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Highscore;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class HighscoreController extends Controller
 {
